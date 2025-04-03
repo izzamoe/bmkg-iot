abc6ec6b5595e4c5e79054612f6563daf7ad6c8c
Izzamuddin Royhul Firdaus <dimanton1221@gmail.com>
2025-04-03 19:18:28 +0700

    feat: add migration to create default superuser with admin credentials

---
+++ migrations/1743682438_superuser_default.go
@@ -0,0 +1,28 @@
+package migrations
+
+import (
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		superusers, err := app.FindCollectionByNameOrId(core.CollectionNameSuperusers)
+		if err != nil {
+			return err
+		}
+
+		record := core.NewRecord(superusers)
+		record.Set("email", "admin@admin.com")
+		record.Set("password", "adminadmin")
+
+		return app.Save(record)
+	}, func(app core.App) error { // optional revert operation
+		record, _ := app.FindAuthRecordByEmail(core.CollectionNameSuperusers, "admin@admin.com")
+		if record == nil {
+			return nil // probably already deleted
+		}
+
+		return app.Delete(record)
+	})
+}
